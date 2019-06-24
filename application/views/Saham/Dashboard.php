730bba2826d86f07052333a80c9bb22b30a578c1
Majidil Khithar <nuralimajids@gmail.com>
2019-06-24 09:26:20 +0700

    up

--- application/views/Saham/Dashboard.php
+++ application/views/Saham/Dashboard.php
@@ -1,4 +1,3 @@
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,10 +7,8 @@
   <link rel="stylesheet" href="style.css">
   <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
   <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
-  <script src="assets/chart/Chart.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.min.css">
 
+  <script src="assets/chart/Chart.js"></script>
 
   <style>
   body{
@@ -55,339 +52,106 @@
   .navbar {
     margin-left: 180px; /* Same as the width of the sidenav */
     padding: 0px 10px;
-    height: 30px;
+    height: 50px;
     background: white;
   }
 
   .text{
     padding-top: 0px;
     font-size: 17px;
-    margin-top: 8px;
   }
 
-  .container{
-    background: white;
-    margin-top: 20px;
+  @media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
   }
 
+  .btn{
+    float: right;
+  }
+  .btn:hover{
+    color: #0056b3;
+  }
 
 </style>
 </head>
 <body>
-<!--   <nav class="navbar navbar-expand-md">
+  <nav class="navbar navbar-expand-md">
+    <div>
+      <button class="btn" onclick="window.location.href='<?=base_url()?>index.php/Dashboard/inputan'"><i class="fas fa-plus"></i>Input Data</button>
+    </div>
   </nav>
-  <hr> -->
+  <hr>
 </body>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.js"></script>
-<!-- Sidebar section -->
 <div class="sidenav">
-  <div class="logo" style="padding: 10px 10px 6px 16px;">
-   <h2>SAHAM</h2>
- </div>
- <a href="<?=base_url()?>index.php/Dashboard/DataEmiten"><i class="fas fa-archive"></i> Data Emiten</a>
- <a href="#"><i class="fas fa-plus"></i>  Input Data</a>
- <a href="<?=base_url()?>index.php/Dashboard/DetailEmiten"><i class="fas fa-plus"></i> Detail Emiten</a>
+  <h2>SAHAM</h2>
+  <a href="#"><i class="fas fa-archive"></i> Data Emiten</a>
+  <a href="#"><i class="fas fa-plus"></i> Input Data</a>
 </div>
-
-<!-- Main section -->
-<style type="text/css">
-
-#addDataButton{
-  position:absolute;
-  top:20px;
-  right:5px;
-}
-</style> 
 <div class="main">
   <div class="col-lg-12" ">
     <div class="row" style="padding-bottom: 10px;">
-      <div class="col" style="background-color:white;">
+      <div class="col-sm-6" style="background-color:white;">
         <h3>BBNI</h3>
         <P class="text">Bank Negara Indonesia tbk</P>
       </div>
-      <div class="col-sm-6 text-center" style="background-color:white; padding-left: 12px;">
-        <h3>Sektor</h3>
-        <P style="font-size: 20px; color: blue;" class="text">Finance</P>
+      <div class="col-sm-6" style="background-color:white;">
+        <P style="text-align: right; padding-right: 20px;" class="text">Sektor: Finance</P>
       </div>
-      <div class="col" style="background-color:white; padding-left: 12px;">
-        <a href="http://localhost/Formulashm/index.php/Dashboard/InputDataForm">
-          <button  type="button" id="addDataButton" class="btn btn-info btn-sm">Add Data Emiten</button></a>
+      <br>
+    </div>
+
+    <div class="row" >
+      <div class="col-sm-6" style="background-color:white;">
+        <div style="width: 800px;margin: 0px auto;">
+          <canvas id="myChart"></canvas>
         </div>
-        <br>
       </div>
-
-      <div class="row">
-        <div class="col" style="background-color:white;">
-          <P class="text">Saham Hari Ini</P>
-          <div class="row">
-            <div class="col">
-             <p style="margin-top: -15px; padding-top: 0px;">Rp.</p>
-           </div>
-           <div class="col">
-             <p style="margin-top: -15px; padding-top: 0px;">1.200</p>
-           </div>
-         </div>
-       </div>
-       <div class="col" style="background-color:white;">
-        <P class="text">Kapitalisasi Pasar</P>
-        <div class="row">
-          <div class="col">
-           <p style="margin-top: -15px; padding-top: 0px;">Rp.</p>
-         </div>
-         <div class="col">
-           <p style="margin-top: -15px; padding-top: 0px;">284.397.000.000.000</p>
-         </div>
-       </div>
-     </div>
-     <div class="col" style="background-color:white;">
-      <P class="text">Saham Beredar</P>
-      <div class="row">
-        <div class="col">
-         <p style="margin-top: -15px; padding-top: 0px;">300.000.000</p>
-       </div>
-       <div class="col">
-         <p style="margin-top: -15px; font-size: 20px; padding-top: 0px; text-align: right;">lembar</p>
-       </div>
-     </div>
-   </div>
- </div>
-
- <div class="container content"">
-  <h4 style="padding-top: 12px;"></h4>
-  <div class="row" style="padding-bottom: 20px;">
-    <div class="col-sm-6" style="background-color:white; ">
-      <div style="width: 500px;margin: 0px auto;">
-        <P>Saham</P>
-        <canvas id="myChart"></canvas>
+      <div class="col-sm-6" style="background-color:white;">
+        <P class="text">Sektor: Finance</P>
       </div>
+      
     </div>
-    <div class="col-sm-6" style="background-color:white;">
-      <P>Neraca</P>
-      <div style="width: 500px;margin: 0px auto;">
-        <canvas id="myChart1"></canvas>
-      </div>
-    </div>
+
   </div>
 </div>
-
-<div class="container content"">
-  <h4 style="padding-top: 12px;"></h4>
-  <div class="row" style="padding-bottom: 20px;">
-    <div class="col-sm-8" style="background-color:white; ">
-      <div style="width: 100%;margin: 0px auto;">
-        <P>Nilai Saham</P>
-        <canvas id="grafikBatang1"></canvas>
-      </div>
-    </div>
-    <div class="col" style="background-color:white;">
-      <div style="padding-top: 50px; padding-right: 4px;">
-        <h5>Hitung Selisih</h5>
-        <div class="input-group date">
-          <input placeholder="Pilih Tanggal" type="text" class="form-control datepicker">
-          <i class="fas fa-calendar-alt" style="padding-left: 5px;"></i>
-        </div>
-        <div class="input-group date" style="padding-top: 5px; padding-bottom: 10px;">
-          <input placeholder="Pilih Tanggal" type="text" class="form-control datepicker">
-          <i class="fas fa-calendar-alt" style="padding-left: 5px;"></i>
-        </div>
-        <h2 class="text-center" style="color: blue;">-54%</h2>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-<!-- Selisih section-->
-
-<div class="container content">
-  <h4 style="padding-top: 12px">Selisih</h4>
-  <div class="row">
-    <div class="col" style="background:white;">
-      <div style="height: 150px;">
-      </div>
-    </div>
-    <div class="col" style="background: white;">
-      <P>Persentase (%)</P>
-      <div style="width: 500px;margin: 0px auto;">
-        <canvas id="grafikBatang"></canvas>
-      </div>
-    </div>      
-  </div>
-</div>
-
-
-
-<script type="text/javascript">
-  $(function(){
-    $(".datepicker").datepicker({
-      format: 'yyyy-mm-dd',
-      autoclose: true,
-      todayHighlight: true,
-    });
-  });
-</script>
-
-</div>
-</div>
-
-
 
 <script>
-  new Chart(document.getElementById("grafikBatang"), {
-    type: 'bar',
-    data: {
-      labels: ["2016", "2017", "2018", "2019"],
-      datasets: [{
-        label: "Profit",
-        type: "line",
-        borderColor: "#8e5ea2",
-        data: [408,547,675,734],
-        fill: false
-      }, {
-        label: "Revenue",
-        type: "line",
-        borderColor: "#3e95cd",
-        data: [133,221,783,2478],
-        fill: false
-      }, {
-        label: "Profit",
-        type: "bar",
-        backgroundColor: "rgba(0,0,0,0.2)",
-        data: [408,547,675,734],
-      }, {
-        label: "Revenue",
-        type: "bar",
-        backgroundColor: "rgba(0,0,0,0.2)",
-        backgroundColorHover: "#3e95cd",
-        data: [133,221,783,2478]
-      }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Population growth (millions): Europe & Africa'
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 23, 2, 3],
+          backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
       },
-      legend: { display: false }
-    }
-  });
-
-  var ctx = document.getElementById("grafikBatang1").getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [{
-        label: '# Jumlah Survei',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
       }
-    }
-  });
-</script>
-
-
-
-<script>
-  var ctx = document.getElementById("myChart").getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ["2018","2018","2018","2019"],
-      datasets: [{
-        label: '# Liabilitas',
-        data: [12, 19, 3, 23, 2, 3],
-        backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
-      }
-    }
-  });
-
-  var ctxm = document.getElementById("myChart1").getContext('2d');
-  var myChart1 = new Chart(ctxm, {
-    type: 'line',
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 23, 2, 3],
-        backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
-      }
-    }
-  });
-
-</script>
+    });
+  </script>
