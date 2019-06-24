2c6e5fb248803a6d4b2e6aa4260df534970ac7f2
Majidil Khithar <nuralimajids@gmail.com>
2019-06-24 09:38:34 +0700

    up

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,32 +2,21 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
-	function __construct(){
-		parent::__construct();
-
-		$this->load->helper(array('form','url'));
-		$this->load->library('form_validation');
-	}
 	public function index()
 	{
-		// $this->load->view('Saham/header');
 		$this->load->view('Saham/Dashboard');
 	}
-
 	public function DataEmiten()
 	{
 		$this->load->view('Saham/DataEmitenView');
 	}
 	public function DetailEmiten()
-
 	{
-		$this->load->view('Saham/input');
+		$this->load->view('Saham/DetailEmitenView');
 	}
-
 
 	public function FormInput()
 	{
 		$this->load->view('Saham/FormInputView');
 	}
-
 }
--- application/views/Saham/Dashboard.php
+++ application/views/Saham/Dashboard.php
@@ -7,75 +7,6 @@
   <link rel="stylesheet" href="style.css">
   <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
   <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
-
-
-  <script src="assets/chart/Chart.js"></script>
-
-  <style>
-  body{
-    background: #f7f6f3;
-  }
-  .sidenav {
-    height: 100%;
-    width: 180px;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    background-color: #1E7373;
-    overflow-x: hidden;
-    padding-top: 20px;
-  }
-
-  .sidenav a {
-    padding: 6px 8px 2px 12px;
-    text-decoration: none;
-    font-size: 16px;
-    color: #fff;
-    display: block;
-  }
-
-  .sidenav a:hover {
-    color: #F5C639;
-  }
-
-  .sidenav h2{
-    padding: 6px 8px 6px 16px;
-    color: white;
-  }
-
-  .main {
-    margin-left: 180px; /* Same as the width of the sidenav */
-    font-size: 28px; /* Increased text to enable scrolling */
-    padding: 0px 10px;
-  }
-
-  .navbar {
-    margin-left: 180px; /* Same as the width of the sidenav */
-    padding: 0px 10px;
-    height: 50px;
-    background: white;
-  }
-
-  .text{
-    padding-top: 0px;
-    font-size: 17px;
-  }
-
-  @media screen and (max-height: 450px) {
-    .sidenav {padding-top: 15px;}
-    .sidenav a {font-size: 18px;}
-  }
-
-  .btn{
-    float: right;
-  }
-  .btn:hover{
-    color: #0056b3;
-  }
-
-</style>
-
   <script src="assets/chart/Chart.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.min.css">
@@ -84,20 +15,13 @@
 
 </head>
 <body>
-  <nav class="navbar navbar-expand-md">
-    <div>
-      <button class="btn" onclick="window.location.href='<?=base_url()?>index.php/Dashboard/inputan'"><i class="fas fa-plus"></i>Input Data</button>
-    </div>
+<!--   <nav class="navbar navbar-expand-md">
   </nav>
-  <hr>
+  <hr> -->
 </body>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.js"></script>
+<!-- Sidebar section -->
 <div class="sidenav">
-
-  <h2>SAHAM</h2>
-  <a href="#"><i class="fas fa-archive"></i> Data Emiten</a>
-  <a href="#"><i class="fas fa-plus"></i> Input Data</a>
-</div>
-
   <div class="logo" style="padding: 10px 10px 6px 16px;">
    <h2>SAHAM</h2>
  </div>
@@ -107,18 +31,13 @@
 </div>
 
 <!-- Main section -->
-
 <div class="main">
   <div class="col-lg-12" ">
     <div class="row" style="padding-bottom: 10px;">
-      <div class="col-sm-6" style="background-color:white;">
+      <div class="col" style="background-color:white;">
         <h3>BBNI</h3>
         <P class="text">Bank Negara Indonesia tbk</P>
       </div>
-
-      <div class="col-sm-6" style="background-color:white;">
-        <P style="text-align: right; padding-right: 20px;" class="text">Sektor: Finance</P>
-
       <div class="col-sm-6 text-center" style="background-color:white; padding-left: 12px;">
         <h3>Sektor</h3>
         <P style="font-size: 20px; color: blue;" class="text">Finance</P>
@@ -173,11 +92,8 @@
       <div style="width: 500px;margin: 0px auto;">
         <P>Neraca</P>
         <canvas id="myChart"></canvas>
-
       </div>
-      <br>
     </div>
-
     <div class="col-sm-6" style="background-color:white;">
       <P>Pasar</P>
       <div style="width: 500px;margin: 0px auto;">
@@ -187,17 +103,29 @@
   </div>
 </div>
 
-
-    <div class="row" >
-      <div class="col-sm-6" style="background-color:white;">
-        <div style="width: 800px;margin: 0px auto;">
-          <canvas id="myChart"></canvas>
-        </div>
+<div class="container content"">
+  <h4 style="padding-top: 12px;"></h4>
+  <div class="row" style="padding-bottom: 20px;">
+    <div class="col-sm-8" style="background-color:white; ">
+      <div style="width: 100%;margin: 0px auto;">
+        <P>Nilai Saham</P>
+        <canvas id="grafikBatang1"></canvas>
       </div>
-
-      <div class="col-sm-6" style="background-color:white;">
-        <P class="text">Sektor: Finance</P>
-   </div>
+    </div>
+    <div class="col" style="background-color:white;">
+      <div style="padding-top: 50px; padding-right: 4px;">
+        <h5>Hitung Selisih</h5>
+        <div class="input-group date">
+          <input placeholder="Pilih Tanggal" type="text" class="form-control datepicker">
+          <i class="fas fa-calendar-alt" style="padding-left: 5px;"></i>
+        </div>
+        <div class="input-group date" style="padding-top: 5px; padding-bottom: 10px;">
+          <input placeholder="Pilih Tanggal" type="text" class="form-control datepicker">
+          <i class="fas fa-calendar-alt" style="padding-left: 5px;"></i>
+        </div>
+        <h2 class="text-center" style="color: blue;">-54%</h2>
+      </div>
+    </div>
   </div>
 </div>
 
@@ -209,7 +137,6 @@
     <div class="col" style="background:white;">
       <div style="height: 150px;">
       </div>
-      
     </div>
     <div class="col" style="background: white;">
       <P>Persentase (%)</P>
@@ -253,49 +180,8 @@
     </div>     
   </div>
 </div>
-  </div>
+
+
 </div>
-
-
-<script>
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 23, 2, 3],
-          backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero:true
-            }
-          }]
-        }
-      }
-    });
-  </script>
-
 </div>
 <script type="text/javascript" src="assets/table.js"></script>
